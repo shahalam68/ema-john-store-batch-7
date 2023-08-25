1ba023efe1105232208cc6a0eba30c9a811d6911
Soykot Alam <a.k.mshahalam68@gmail.com>
2023-08-25 12:23:43 +0600

    remove from cart

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -3,25 +3,33 @@ import Cart from '../Cart/Cart';
 import { useLoaderData } from 'react-router-dom';
 import ReviewItem from '../ReviewItem/ReviewItem';
 import './Orders.css';
+import { removeFromDb } from '../../utilities/fakedb';
 const Orders = () => {
-    const savedCaart = useLoaderData();
-    const [cart,setCart] = useState(savedCaart);
+    const savedCart = useLoaderData();
+    const [cart,setCart] = useState(savedCart);
 
 
+    const handleRemoveFromCart = (id) =>{
+        console.log(id);
+        const remaining =  cart.filter(product => product.id !== id);
+        setCart(remaining);
+        removeFromDb(id);
+    }
 
     return (
         <div className='shop-container'>
             <div className='review-container'>
                 {
-                    savedCaart.map(product => <ReviewItem 
+                    cart.map(product => <ReviewItem 
                     key={product.id}
                     product={product}
+                    handleRemoveFromCart={handleRemoveFromCart}
                    ></ReviewItem>)
                 }
 
             </div>
             <div className='cart-container'>
-                <Cart cart={savedCaart}></Cart>
+                <Cart cart={cart}></Cart>
             </div>
         </div>
     );
